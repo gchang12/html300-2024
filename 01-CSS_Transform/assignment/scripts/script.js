0593b219df7044c818665bcd7c97d0ede26bb8d5
gchang12 <gc8@hawaii.edu>
2026-07-10 17:59:49 -1000

    I changed the snippet I want to edit.

--- 01-CSS_Transform/assignment/scripts/script.js
+++ 01-CSS_Transform/assignment/scripts/script.js
@@ -1,45 +1,14 @@
-'use strict'
+const receipt = document.querySelector('.receipt')
+const button = document.querySelector('button')
 
-/* https://codepen.io/kotAndy/pen/MxKVOj */
-
-const nav = document.querySelector('#navigation');
-const triggers = document.querySelectorAll('[data-dropdown]');
-const dropdownBG = document.querySelector('.dropdown-bg');
-
-function handleEnter() {
-  this.classList.add('trigger-enter');
-  setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter--active'), 200);
-  dropdownBG.classList.add('active');
-  
-  const dropdown = this.querySelector('.dropdown');
-  const pos = dropdown.dataset.position;
-  const coordsDropdown = dropdown.getBoundingClientRect();
-  const coordsNav = nav.getBoundingClientRect();
-  
-  const coords = {
-    width: coordsDropdown.width,
-    height: coordsDropdown.height,
-    top: coordsDropdown.top - coordsNav.top,
-    left: coordsDropdown.left
-  }
-  
-  dropdownBG.style.setProperty('width', `${coords.width}px`);
-  dropdownBG.style.setProperty('height', `${coords.height}px`);
-  dropdownBG.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
-  (pos) && dropdownBG.classList.add(pos);	
+const toggleReceipt= () => {
+	receipt.classList.toggle('active')
+	
+	if(receipt.classList.contains('active')) {
+		button.innerHTML = 'Less info'
+	} else {
+		button.innerHTML = 'More info'
+	}
 }
 
-function handleLeave(e) {
-  if (e.target.matches('html')) {
-    triggers.forEach(trigger => trigger.classList.remove('trigger-enter', 'trigger-enter--active'));
-  } else {
-    this.classList.remove('trigger-enter', 'trigger-enter--active');
-  }
-  
-  dropdownBG.classList.remove('active');
-  dropdownBG.classList.contains('left') && dropdownBG.classList.remove('left');
-}
-
-triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
-triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
-window.addEventListener('touchstart', handleLeave);
+button.addEventListener('click', toggleReceipt)
