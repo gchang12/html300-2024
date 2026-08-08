a46f1a4c7876a81675f6e04d7756cf705f35576d
gchang12 <gc8@hawaii.edu>
2026-08-07 20:30:08 -1000

    Added skeleton for routing.

--- 04-Intro_to_Vue/assignment/lesson07/src/App.vue
+++ 04-Intro_to_Vue/assignment/lesson07/src/App.vue
@@ -1,10 +1,3 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
@@ -14,7 +7,45 @@ export default {
     HelloWorld
   }
 }
+
+/* Imported from video demonstration. */
+
+/*
+import { ref, computed } from "vue";
+import Home from "./components/Home.vue";
+import About from "./components/About.vue";
+import NotFound from "./components/NotFound.vue";
+
+const routes = {
+  "/": Index,
+  "/landing-page": LandingPage,
+  "/images": Images,
+  "/accordion": Accordion,
+  "/grid-system": GridSystem,
+}
+
+const currentPath = ref(window.location.hash);
+
+window.addEventListener("hashchange", () => {
+  currentPath.value = window.location.hash;
+});
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || "/"] || NotFound;
+});
+*/
+
+/* Imported from video demonstration. */
+
 </script>
+
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <component :is="currentView" />
+  </div>
+</template>
 
 <style>
 #app {
