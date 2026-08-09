5b2dfd690a33061f2aa141728881f52036ecd32f
gchang12 <gc8@hawaii.edu>
2026-08-08 16:21:42 -1000

    Laid out content for accordion page

--- 04-Intro_to_Vue/assignment/lesson07/src/components/AccordionPage.vue
+++ 04-Intro_to_Vue/assignment/lesson07/src/components/AccordionPage.vue
@@ -1,89 +1,69 @@
-<script>
+<script setup>
+  import { ref } from 'vue';
+  const definitions = ref([
+    {
+      word: "mercurial",
+      cardHeader: "headingOne",
+      ariaControls: "collapseOne",
+      dataToggle: "#collapseOne",
+      definitions: [
+        ["noun", "Any of the plants known as mercury, especially the annual mercury or French mercury (Mercurialis annua)."],
+        ["noun", "A person born under the influence of the planet Mercury; hence, a person having an animated, lively, quick-witted or volatile character."],
+        ["noun", "A chemical compound containing mercury."],
+        ["noun", "A preparation of mercury, especially as a treatment for syphilis."],
+        ["adjective", "Having a lively or volatile character; animated, changeable, quick-witted."],
+        ["adjective", "Pertaining to the astrological influence of the planet Mercury; having the characteristics of a person under such influence (see adjective sense 1)."],
+        ["adjective", "Pertaining to the planet Mercury."],
+        ["adjective", "Of or pertaining to the element mercury or quicksilver; containing mercury."],
+        ["adjective", "Caused by the action of mercury or a mercury compound."],
+        ["adjective", "Pertaining to Mercury, the Roman god of, among other things, commerce, financial gain, communication, and thieves and trickery; hence , money-making; crafty."],
+      ],
+    },
+    {
+      word: "verisimilitude",
+      cardHeader: "headingTwo",
+      ariaControls: "collapseTwo",
+      dataToggle: "#collapseTwo",
+      definitions: [
+        ["noun", "The property of seeming true, of resembling reality; resemblance to reality, realism."],
+        ["noun", "A statement which merely appears to be true."],
+        ["noun", "Faithfulness to its own rules; internal cohesion."],
+      ],
+    },
+    {
+      word: "reminisce",
+      cardHeader: "headingThree",
+      ariaControls: "collapseThree",
+      dataToggle: "#collapseThree",
+      definitions: [
+        ["noun", "An act of reminiscence."],
+        ["verb", "To recall the past in a private moment, often fondly or nostalgically."],
+        ["verb", "To talk or write about memories of the past, especially pleasant memories."],
+        ["verb", "To remember fondly; to reminisce about."],
+      ],
+    }
+  ]);
 </script>
 
 <template>
   <div id="accordion">
     <!-- Implemented accordion content structure here. -->
-    <div class="card">
-      <div class="card-header" id="headingOne">
+    <div class="card" v-for="definition in definitions" :key="definition.word">
+      <div class="card-header" :id="definition.cardHeader">
         <!-- NOTE: Added tooltip here! -->
         <h5 class="mb-0" data-toggle="tooltip" title="Click me for a definition!">
           <!-- Press to activate accordion -->
-          <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-            mercurial
+          <button class="btn btn-link" data-toggle="collapse" :data-target="definition.dataToggle" aria-expanded="true" :aria-controls="definition.ariaControls">
+            {{ definition.word }}
           </button>
         </h5>
       </div>
       <!-- Conditionally renders -->
-      <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+      <div :id="definition.cardHeader" class="collapse show" :aria-labelledby="definition.cardHeader" data-parent="#accordion">
         <div class="card-body">
-          <dl>
-            <dt>noun</dt>
-            <dd>Any of the plants known as mercury, especially the annual mercury or French mercury (Mercurialis annua).</dd>
-            <dt>noun</dt>
-            <dd>A person born under the influence of the planet Mercury; hence, a person having an animated, lively, quick-witted or volatile character.</dd>
-            <dt>noun</dt>
-            <dd>A chemical compound containing mercury.</dd>
-            <dt>noun</dt>
-            <dd>A preparation of mercury, especially as a treatment for syphilis.</dd>
-            <dt>adjective</dt>
-            <dd>Having a lively or volatile character; animated, changeable, quick-witted.</dd>
-            <dt>adjective</dt>
-            <dd>Pertaining to the astrological influence of the planet Mercury; having the characteristics of a person under such influence (see adjective sense 1).</dd>
-            <dt>adjective</dt>
-            <dd>Pertaining to the planet Mercury.</dd>
-            <dt>adjective</dt>
-            <dd>Of or pertaining to the element mercury or quicksilver; containing mercury.</dd>
-            <dt>adjective</dt>
-            <dd>Caused by the action of mercury or a mercury compound.</dd>
-            <dt>adjective</dt>
-            <dd>Pertaining to Mercury, the Roman god of, among other things, commerce, financial gain, communication, and thieves and trickery; hence , money-making; crafty.</dd>
-          </dl>
-        </div>
-      </div>
-    </div>
-    <div class="card">
-      <div class="card-header" id="headingTwo">
-        <!-- NOTE: Added tooltip here! -->
-        <h5 class="mb-0" data-toggle="tooltip" title="Click me for a definition!">
-          <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-            verisimilitude
-          </button>
-        </h5>
-      </div>
-      <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-        <div class="card-body">
-          <dl>
-            <dt>noun</dt>
-            <dd>The property of seeming true, of resembling reality; resemblance to reality, realism.</dd>
-            <dt>noun</dt>
-            <dd>A statement which merely appears to be true.</dd>
-            <dt>noun</dt>
-            <dd>Faithfulness to its own rules; internal cohesion.</dd>
-          </dl>
-        </div>
-      </div>
-    </div>
-    <div class="card">
-      <div class="card-header" id="headingThree">
-        <!-- NOTE: Added tooltip here! -->
-        <h5 class="mb-0" data-toggle="tooltip" title="Click me for a definition!">
-          <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-            reminisce
-          </button>
-        </h5>
-      </div>
-      <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-        <div class="card-body">
-          <dl>
-            <dt>noun</dt>
-            <dd>An act of reminiscence.</dd>
-            <dt>verb</dt>
-            <dd>To recall the past in a private moment, often fondly or nostalgically.</dd>
-            <dt>verb</dt>
-            <dd>To talk or write about memories of the past, especially pleasant memories.</dd>
-            <dt>verb</dt>
-            <dd>To remember fondly; to reminisce about.</dd>
+          <dl v-for="([partOfSpeech, defText]) in definition.definitions" :key="defText">
+            <dt>{{partOfSpeech}}</dt>
+            <dd>{{defText}}</dd>
           </dl>
         </div>
       </div>
