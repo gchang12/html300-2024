c6c2577b4774ab63d3070bd52b7a59a7d9f5fe6e
gchang12 <gc8@hawaii.edu>
2026-08-09 09:49:00 -1000

    Made image gallery a bit more responsive

--- 04-Intro_to_Vue/assignment/lesson07/src/components/ImageGallery.vue
+++ 04-Intro_to_Vue/assignment/lesson07/src/components/ImageGallery.vue
@@ -16,16 +16,20 @@
 
 <template>
   <div id="image-gallery">
-    <figure v-for="image in images" :key="image.src">
-      <!-- img-fluid class is added to ensure that image is responsive -->
-      <img class="img-fluid" :src="image.src" :alt="image.alt" />
-      <figcaption>{{image.alt}}
-      <!-- Image attribution here, within figure caption. -->
-      </figcaption>
-    </figure>
+    <div id="figures">
+      <figure v-for="image in images" :key="image.src">
+        <!-- img-fluid class is added to ensure that image is responsive -->
+        <img class="img-fluid" :src="image.src" :alt="image.alt" />
+        <figcaption>{{image.alt}}
+        <!-- Image attribution here, within figure caption. -->
+        </figcaption>
+      </figure>
+    </div>
     <!-- https://openverse.org/image/a139ef61-0ef7-480c-a07f-441129b209a7?q=polar+night&p=8 -->
-    <p class="attribution">"<a rel="noopener noreferrer" href="https://www.flickr.com/photos/38144390@N04/4395753165">Dusk at the dawn of the polar night</a>" by <a rel="noopener noreferrer" href="https://www.flickr.com/photos/38144390@N04">Bernt Sønvisen</a> is licensed under <a rel="noopener noreferrer" href="https://creativecommons.org/licenses/by/2.0/?ref=openverse">CC BY 2.0 <img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" style="height: 1em; margin-right: 0.125em; display: inline;" /><img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" style="height: 1em; margin-right: 0.125em; display: inline;" /></a>.</p>
-    <!-- https://openverse.org/image/62203ec4-9155-4089-9d2c-a008c8ea092b?q=antarctica&p=21 -->
-    <p class="attribution">"<a rel="noopener noreferrer" href="https://www.flickr.com/photos/10978010@N07/12434752073">Antarctica sky and ice</a>" by <a rel="noopener noreferrer" href="https://www.flickr.com/photos/10978010@N07">londoncyclist</a> is licensed under <a rel="noopener noreferrer" href="https://creativecommons.org/licenses/by/2.0/?ref=openverse">CC BY 2.0 <img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" style="height: 1em; margin-right: 0.125em; display: inline;" /><img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" style="height: 1em; margin-right: 0.125em; display: inline;" /></a>.</p>
+    <div id="attributions">
+      <p class="attribution">"<a rel="noopener noreferrer" href="https://www.flickr.com/photos/38144390@N04/4395753165">Dusk at the dawn of the polar night</a>" by <a rel="noopener noreferrer" href="https://www.flickr.com/photos/38144390@N04">Bernt Sønvisen</a> is licensed under <a rel="noopener noreferrer" href="https://creativecommons.org/licenses/by/2.0/?ref=openverse">CC BY 2.0 <img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" style="height: 1em; margin-right: 0.125em; display: inline;" /><img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" style="height: 1em; margin-right: 0.125em; display: inline;" /></a>.</p>
+      <!-- https://openverse.org/image/62203ec4-9155-4089-9d2c-a008c8ea092b?q=antarctica&p=21 -->
+      <p class="attribution">"<a rel="noopener noreferrer" href="https://www.flickr.com/photos/10978010@N07/12434752073">Antarctica sky and ice</a>" by <a rel="noopener noreferrer" href="https://www.flickr.com/photos/10978010@N07">londoncyclist</a> is licensed under <a rel="noopener noreferrer" href="https://creativecommons.org/licenses/by/2.0/?ref=openverse">CC BY 2.0 <img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" style="height: 1em; margin-right: 0.125em; display: inline;" /><img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" style="height: 1em; margin-right: 0.125em; display: inline;" /></a>.</p>
+    </div>
   </div>
 </template>
