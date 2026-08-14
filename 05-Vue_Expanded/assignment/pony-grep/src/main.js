be348f1960df523887480d58829492ef8add472f
gchang12 <gc8@hawaii.edu>
2026-08-13 14:23:07 -1000

    Referenced 04-Intro_to_Vue/assignment/README.md to create new project.

---
+++ 05-Vue_Expanded/assignment/pony-grep/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// Making Bootstrap available in Vue project.
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+// Credit: https://bootstrap-vue.org/docs/#using-module-bundlers
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
