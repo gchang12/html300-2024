ed5e843f8edfdd43e0b4ab88b4b0c71479dcf0b8
gchang12 <gc8@hawaii.edu>
2026-08-18 13:08:48 -1000

    Added custom SCSS file

--- 05-Vue_Expanded/assignment/pony-grep/src/main.js
+++ 05-Vue_Expanded/assignment/pony-grep/src/main.js
@@ -6,6 +6,8 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import './app.scss'
+
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 // Credit: https://bootstrap-vue.org/docs/#using-module-bundlers
