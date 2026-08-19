71dfba4a58838d8cec60fbc959050b0db9d7ab1d
gchang12 <gc8@hawaii.edu>
2026-08-18 18:24:35 -1000

    Figured out the styling of the index... for the most part.

--- 05-Vue_Expanded/assignment/pony-grep/src/components/EpisodeDirectory.vue
+++ 05-Vue_Expanded/assignment/pony-grep/src/components/EpisodeDirectory.vue
@@ -8,51 +8,65 @@
   const currentEpisode = ref({});
   const activeSeason = ref(-1);
 
-  /*
   function setSeason(e) {
-    console.log(e);
-    const seasonNo = e.target.dataset.season;
-    if (activeSeason.value !== seasonNo) {
+    //console.log(e);
+    const seasonNo = e.currentTarget.dataset.season;
+    if (activeSeason.value != seasonNo) {
       activeSeason.value = seasonNo;
     } else {
       activeSeason.value = -1;
     }
+    console.log(activeSeason);
   }
-  */
 
+  /*
   // TODO: Use for all episode transcript directories and leverage urlParams
   function selectEpisode(e) {
     const id = e.currentTarget.dataset.id;
     currentEpisode.value = episodeList.find(episode => episode.id == id);
   }
+  */
 
 </script>
 
 <template>
   <div id="episode-directory">
     <h1>Transcript Viewer</h1>
+
     <div class="container">
       <div class="row">
+
         <div class="col">
           <h2>Index</h2>
-          <aside class="EpisodeIndex">
+          <aside id="mlp-fim" class="EpisodeIndex">
             <h3>MLP:FiM</h3>
-            <ol>
-              <li class="SeasonList" v-for="season in [1, 2, 3, 4, 5, 6, 7, 8, 9]" :key="season">
-                <a :href="'/#/transcripts/FiM/S' + season">
-                  S{{ season }}
+            <ol class="navbar-nav">
+              <!-- <li class="nav-item dropdown" v-for="seasonNo in [1, 2, 3, 4, 5, 6, 7, 8, 9]" :key="seasonNo"> -->
+              <li class="nav-item" v-for="seasonNo in [1, 2, 3, 4, 5, 6, 7, 8, 9]" :key="seasonNo">
+                <!-- <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false"> -->
+                <a class="nav-link" role="button">
+                  <!-- TODO: Dropdown not working. Temporary fix. -->
+                  <button class="btn btn-primary" :data-season="seasonNo" @click="setSeason">
+                    S{{seasonNo}}
+                  </button>
                 </a>
-                <ol v-if="activeSeason == season" class="season">
-                  <li class="episode" v-for="episode in episodeList.filter(episode => episode.seasonNo === season)" :key="episode.id">
-                    <button @click="selectEpisode" :data-id="episode.id">
-                      {{ episode.title }}
-                    </button>
+                <!-- <div v-if="seasonNo == activeSeason"> Active Season: {{ activeSeason }} </div> -->
+                <!-- <ol class="dropdown-menu" v-if="seasonNo == activeSeason"> -->
+                <ol class="navbar-nav" v-if="seasonNo == activeSeason">
+                  <li class="nav-item align-items-start justify-content-start" v-for="episode in episodeList.filter(episode => episode.seasonNo == seasonNo)" :key="episode.id">
+                    <!-- <a role="button" class="dropdown-item"> -->
+                    <a class="EpisodeEntry" role="button">
+                      <!-- <button class="btn btn-secondary">E{{ episode.episodeNo }}</button>{{ episode.title }} -->
+                      <button class="btn btn-secondary">E{{ episode.episodeNo }}</button> {{ episode.title }}
+                      <!-- E{{ episode.episodeNo }} {{ episode.title }} -->
+                    </a>
                   </li>
                 </ol>
               </li>
             </ol>
           </aside>
         </div>
+
         <div class="col">
           <main class="transcript">
             <h2>Transcript</h2>
@@ -79,6 +93,7 @@
             </div>
           </main>
         </div>
+
         <div class="col">
           <aside class="current-episode-info">
             <h2>Episode Info</h2>
@@ -104,6 +119,7 @@
             </div>
           </aside>
         </div>
+
       </div>
     </div>
   </div>
