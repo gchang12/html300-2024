a2647ca2e86df26be6b20003fd777ec7fe82af46
gchang12 <gc8@hawaii.edu>
2026-08-20 10:15:25 -1000

    Spruced up about-page.

--- 05-Vue_Expanded/assignment/pony-grep/src/pages/IndexPage.vue
+++ 05-Vue_Expanded/assignment/pony-grep/src/pages/IndexPage.vue
@@ -46,37 +46,46 @@
 
 <template>
   <div id="index">
-    <article class="container w-50">
+    <article class="container w-75">
       <h1>Welcome to <code>ponygrep</code>!</h1>
-      <section>
-        <h2>About Us</h2>
-        <p>Trying to remember if a MLP:FiM character said something?</p>
-        <p>You've come to the right place!
-        <p><code>ponygrep</code> allows you to search all of MLP G4 for the bit of dialgoue you're thinking of.</p>
-        <article class="w-75 container">
-          <h2>Searching</h2>
-          <section>
-            <h3>Example</h3>
-            <ol>
-              <li class="Help">&ldquo;I want to see if &lsquo;friendship is magic&rsquo; is said in MLP G4. I'll input <span class="_fieldValue">friendship is magic</span> into the <span class="_fieldName">Dialogue</span> box now.&rdquo;</li>
-              <li class="Help">&ldquo;I want to see if Discord or Twilight says it. I'll input <span class="_fieldValue">Discord|Twilight</span> into the <span class="_fieldName">Character</span> field.&rdquo;</li> 
-              <li class="Help">&ldquo;Let's see if either Discord or Twilight say &lsquo;friendship is magic&rsquo; by hitting the <span class="_fieldName">Search</span> button.&rdquo;</li>
+      <p>Trying to remember if a MLP:FiM character said something?</p>
+      <p>You've come to the right place!
+      <p><code>ponygrep</code> allows you to search all of MLP G4 for the bit of dialgoue you're thinking of.</p>
+      <hr />
+      <article class="container">
+        <h2>Searching</h2>
+        <section class="Usage-Example">
+          <h3>Example</h3>
+          <div class="Usage-Step d-flex">
+            <img class="img-thumbnail" src="/images/search-form.png" />
+            <ol class="m-5">
+              <li class="Help m-3">&ldquo;I want to see if &lsquo;friendship is magic&rsquo; is said in MLP G4. I'll input <span class="_fieldValue">friendship is magic</span> into the <span class="_fieldName">Dialogue</span> box now.&rdquo;</li>
+              <li class="Help m-3">&ldquo;I want to see if Discord or Twilight says it. I'll input <span class="_fieldValue">Discord|Twilight</span> into the <span class="_fieldName">Character</span> field.&rdquo;</li> 
+              <li class="Help m-3">&ldquo;Let's see if either Discord or Twilight say &lsquo;friendship is magic&rsquo; by hitting the <span class="_fieldName">Search</span> button.&rdquo;</li>
             </ol>
-          </section>
-        </article>
-        <article class="w-75 container">
-          <h2>Browsing Transcripts</h2>
-          <section class="d-flex align-items-center flex-column">
-            <h3>Instructions</h3>
-            <ol>
-              <li>Select season from index.</li>
-              <li>Select episode from episode index.</li>
-              <li>Scroll down and view transcript.</li>
-              <li>Select episode again to close the transcript.</li>
-            </ol>
-          </section>
-        </article>
-      </section>
+          </div>
+        </section>
+      </article>
+      <hr />
+      <article class="BrowsingTranscripts container">
+        <h2>Browsing Transcripts</h2>
+        <section class="BrowsingStep">
+          <h3>Select Season</h3>
+          <img width="500px" class="img-thumbnail" src="/images/season-index.png" />
+        </section>
+        <section class="BrowsingStep">
+          <h3>Select Episode</h3>
+          <img width="500px" class="img-thumbnail" src="/images/episode-index.png" />
+        </section>
+        <section class="BrowsingStep">
+          <h3>Scroll Down and View</h3>
+          <img width="500px" class="img-thumbnail" src="/images/transcript.png" />
+        </section>
+        <section class="BrowsingStep">
+          <h3>Close Transcript</h3>
+          <img width="500px" class="img-thumbnail" src="/images/close-transcript.png" />
+        </section>
+      </article>
       <!-- <article> -->
       <!-- <p>Trying to remember a line that a children's cartoon horse might have said? This is the perfect tool for finding that out!  This website scans every dialogue of if a character from <i>My Little Pony: Friendship is Magic</i> and checks if any character in this show ever said the thing you queried.  Just go on over to <a href="#/">Search</a> and input your query.</p> -->
       <!-- </article> -->
