7f0978a0750386d49b6f81fc478f91c4590248f9
gchang12 <gc8@hawaii.edu>
2026-08-20 15:59:25 -1000

    Replaced some filler values with meaningful ones.

--- 05-Vue_Expanded/assignment/pony-grep/src/components/AccordionResults.vue
+++ 05-Vue_Expanded/assignment/pony-grep/src/components/AccordionResults.vue
@@ -17,12 +17,12 @@
       <h3 class="accordion-header">
         <!-- Bootstrap: 'd-flex', 'justify-content-between' to separate series title and result-count.-->
         <!-- Bootstrap: 'btn-block' so that it spans entire width of container.-->
-        <b-button v-b-toggle="'collapse-' + collapseKey" :data-series="seriesName" class="ShowResults d-flex justify-content-between btn btn-lg btn-block accordion-button" aria-expanded="true" aria-controls="collapseOne">
+        <b-button v-b-toggle="'collapse-' + collapseKey" :data-series="seriesName" class="ShowResults d-flex justify-content-between btn btn-lg btn-block accordion-button" aria-expanded="true" :aria-controls="'collapse-' + collapseKey">
           <slot name="header">
           </slot>
         </b-button>
       </h3>
-      <b-collapse :id="'collapse-' + collapseKey" aria-labelledby="headingOne">
+      <b-collapse :id="'collapse-' + collapseKey" :aria-labelledby="'heading-' + collapseKey">
         <b-card class="SearchResults accordion-body">
           <!-- Bootstrap: 'table-striped' for line visibility. -->
           <table class="table table-primary table-light table-striped">
